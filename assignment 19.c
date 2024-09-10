05e9a1d652e61e78c540d0848140954ed5160082
Suprajakshaya116 <suprajakshayamummadi@gmail.com>
2024-09-10 23:17:02 +0530

    Delete assignment 19.c

--- assignment 19.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
- {
-   char c,y;
-   printf("enter character : \n");
-   scanf("%c",&c);
-   y= (c>= 'a')?( (c<='z') ? (printf("it is small Case letter ")) :printf("it is not a small case letter ")):printf("it is not a small case letter");
-   return 0;
- }
